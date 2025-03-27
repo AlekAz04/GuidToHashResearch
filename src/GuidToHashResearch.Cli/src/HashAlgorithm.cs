7080e49cb47d1caad3097028bd5d0fb8de28e99a
Leonid Vakarin <l.vakarin@intellectika.ru>
2025-03-27 14:07:14 +0300

    Более оптимальная либа хэша

--- src/GuidToHashResearch.Cli/src/HashAlgorithm.cs
+++ src/GuidToHashResearch.Cli/src/HashAlgorithm.cs
@@ -10,10 +10,10 @@ public static class HashAlgorithm
     /// </summary>
     public static string GetStringMD5Hash(string text)
     {
-        using var md5 = new System.Security.Cryptography.HMACMD5();
-        byte[] textData = System.Text.Encoding.UTF8.GetBytes(text);
-        byte[] hash = md5.ComputeHash(textData);
-        return BitConverter.ToString(hash).Replace("-", string.Empty);
+        byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(text);
+        byte[] hashBytes = System.Security.Cryptography.MD5.HashData(inputBytes);
+
+        return Convert.ToHexString(hashBytes);
     }
 
     /// <summary>
@@ -21,10 +21,10 @@ public static class HashAlgorithm
     /// </summary>
     public static string GetStringSha256Hash(string text)
     {
-        using var sha256 = new System.Security.Cryptography.HMACSHA256();
-        byte[] textData = System.Text.Encoding.UTF8.GetBytes(text);
-        byte[] hash = sha256.ComputeHash(textData);
-        return BitConverter.ToString(hash).Replace("-", string.Empty);
+        byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(text);
+        byte[] hashBytes = System.Security.Cryptography.SHA256.HashData(inputBytes);
+
+        return Convert.ToHexString(hashBytes);
     }
 
     /// <summary>
@@ -32,10 +32,10 @@ public static class HashAlgorithm
     /// </summary>
     public static string GetStringSha384Hash(string text)
     {
-        using var sha384 = new System.Security.Cryptography.HMACSHA384();
-        byte[] textData = System.Text.Encoding.UTF8.GetBytes(text);
-        byte[] hash = sha384.ComputeHash(textData);
-        return BitConverter.ToString(hash).Replace("-", string.Empty);
+        byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(text);
+        byte[] hashBytes = System.Security.Cryptography.SHA384.HashData(inputBytes);
+
+        return Convert.ToHexString(hashBytes);
     }
 
     /// <summary>
@@ -43,9 +43,9 @@ public static class HashAlgorithm
     /// </summary>
     public static string GetStringSha512Hash(string text)
     {
-        using var sha512 = new System.Security.Cryptography.HMACSHA512();
-        byte[] textData = System.Text.Encoding.UTF8.GetBytes(text);
-        byte[] hash = sha512.ComputeHash(textData);
-        return BitConverter.ToString(hash).Replace("-", string.Empty);
+        byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(text);
+        byte[] hashBytes = System.Security.Cryptography.SHA512.HashData(inputBytes);
+
+        return Convert.ToHexString(hashBytes);
     }
 }
